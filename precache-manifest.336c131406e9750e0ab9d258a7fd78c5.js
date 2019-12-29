17eda15cae3da24713688284de07742134ed1e72
shriram-mnit <43758704+shriram-mnit@users.noreply.github.com>
2019-12-29 23:14:05 +0530

    Updates

---
+++ precache-manifest.336c131406e9750e0ab9d258a7fd78c5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8418815f45d2f2d1a23b69570782fac",
+    "url": "/robo.github.io/index.html"
+  },
+  {
+    "revision": "b8c242a663d4a617c776",
+    "url": "/robo.github.io/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "6557295b320af1e85134",
+    "url": "/robo.github.io/static/css/main.5f778e1b.chunk.css"
+  },
+  {
+    "revision": "b8c242a663d4a617c776",
+    "url": "/robo.github.io/static/js/2.c7991b8a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robo.github.io/static/js/2.c7991b8a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6557295b320af1e85134",
+    "url": "/robo.github.io/static/js/main.0cde22fe.chunk.js"
+  },
+  {
+    "revision": "9588a4eb15056439c8f2",
+    "url": "/robo.github.io/static/js/runtime-main.0180257a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo.github.io/static/media/SEGA.12a3db8b.woff"
+  }
+]);
